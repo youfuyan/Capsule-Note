256c831c521189ee510401724950ee84d85c616f
YoufuYAN <arnoldyyf@gmail.com>
2023-04-12 16:16:24 -0500

    editor.js and css using quill.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
-
+import '@/styles/globals.css';
+import 'react-quill/dist/quill.snow.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
