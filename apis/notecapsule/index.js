66c4062016daf0855f910cf3ae3a41ec0c75e81a
Marcellinus Sugiarto <sugia005@umn.edu>
2023-04-19 16:53:46 -0500

    fix db

--- apis/notecapsule/index.js
+++ apis/notecapsule/index.js
@@ -5,7 +5,7 @@ import jwtDecode from 'jwt-decode';
 import * as Yup from 'yup';
 
 // Define the schema for a Todo object using Yup
-const noteSchema = Yup.object().shape({
+const noteSchema = Yup.object({
   userId: Yup.string().required('User ID is required'), // User ID
   title: Yup.string().required('Note title is required'), // note title
   content: Yup.string().required('Note content is required'), // note content
@@ -33,15 +33,34 @@ const userAuth = async (req, res, next) => {
     if (authorization) {
       const token = authorization.replace('Bearer ', '');
       // NOTE this doesn't validate, but we don't need it to. codehooks is doing that for us.
+      
       const token_parsed = jwtDecode(token);
       req.user_token = token_parsed;
     }
     next();
   } catch (error) {
-    next(error);
+    console.log(error);
+    res.status(400);
+    res.json(error).end();
+    next();
   }
 };
 app.use(userAuth);
+
+app.use("/note", (req, res, next) => {
+  if (req.method === "POST") {
+    // always save authenticating user Id token.
+    // note -- were not enforcing uniqueness which isn't great.
+    // we don't currently have a great way to do this -- one option would be to
+    // have a user collection track which collections have been filled
+    // It's a limitation for sure, but I'll just make that a front-end problem...
+    req.body.userId = req.user_token.sub;
+  } else if (req.method === "GET") {
+    // on "index" -- always check for authentication.
+    req.query.userId = req.user_token.sub;
+  }
+  next();
+});
 
 // lastest note is on the top
 async function getNotesDescSortedByDate(req, res) {
@@ -72,7 +91,6 @@ async function getNotesAescSortedByDate(req, res) {
 }
 
 // Make REST API CRUD operations for the "notes" collection with the Yup schema
-crudlify(app, { note: noteSchema }, options);
 
 // search the note table by keyword
 // show results by date desc order
@@ -97,6 +115,17 @@ async function getSearchRes(req, res) {
   };
   conn.find('note', options).json(res);
 }
+
+async function getNotes(req, res) {
+  const userId = req.user_token.sub;
+  const conn = await Datastore.open();
+  const options = {
+    filter: { userId: userId },
+  };
+  conn.getMany("note", options).json(res);
+}
+
+app.get('/note', getNotes);
 
 app.get('/getAllNotesDesc', getNotesDescSortedByDate);
 
